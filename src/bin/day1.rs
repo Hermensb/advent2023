cdb1f5d717a1a0f76c6f5fa75250520813a03934
Brad Hermens <brad.hermens@gmail.com>
2023-12-01 23:00:24 -0800

    solve day 1 part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,7 +2,11 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("data/day1").expect("Didn't find the file?");
-    let numbers = data.split('\n').map(|x| get_num(x));
+    let numbers = data
+        .split('\n')
+        .filter(|x| x.len() > 0)
+        .map(|x| replace_word_digits(x))
+        .map(|x| get_num(&x));
     let total: u32 = numbers.sum();
     println!("Total: {}", total);
 }
@@ -28,6 +32,25 @@ fn get_num(input: &str) -> u32 {
     (first * 10) + last
 }
 
+fn replace_word_digits(input: &str) -> String {
+    input
+        .replace("oneight", "18")
+        .replace("threeight", "38")
+        .replace("fiveight", "58")
+        .replace("nineight", "98")
+        .replace("eightwo", "82")
+        .replace("twone", "21")
+        .replace("one", "1")
+        .replace("two", "2")
+        .replace("three", "3")
+        .replace("four", "4")
+        .replace("five", "5")
+        .replace("six", "6")
+        .replace("seven", "7")
+        .replace("eight", "8")
+        .replace("nine", "9")
+}
+
 #[test]
 fn test_get_num() {
     assert_eq!(75, get_num("kkyd7ki89sldicy5klk"));
@@ -36,4 +59,9 @@ fn test_get_num() {
 #[test]
 fn test_empty() {
     assert_eq!(0, get_num(""));
+}
+
+#[test]
+fn test_convert() {
+    assert_eq!("4xv56yy", &replace_word_digits("fourxvfivesixyy"));
 }
