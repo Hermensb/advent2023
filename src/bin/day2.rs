c9f9bcc6dd9e36bdd12ed43789add87a9983d318
Brad Hermens <brad.hermens@gmail.com>
2023-12-02 21:54:44 -0800

    day 2 part 2 complete

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,9 +3,9 @@ use std::fs;
 fn main() {
     let data = fs::read_to_string("data/day2").expect("Didn't find the file?");
     let lines = data.split('\n').filter(|x| x.len() > 0);
-    let games = lines.map(|x| Game::new(x)).filter(|x| x.check_if_impossible((12,13,14)));
-    let total: u32 = games.map(|x| x.number).sum();
-    print!("Total: {}", total);
+    let games = lines.map(|x| Game::new(x)).map(|x| x.get_power());
+    let total: u32 = games.sum();
+    print!("Total {}", total);
 }
 
 #[derive(Debug, PartialEq)]
@@ -47,6 +47,18 @@ impl Game {
             }
         }
         true
+    }
+
+    fn get_fewest_possible(&self) -> (u8, u8, u8) {
+        let min_red = &self.samples.iter().map(|x| x.0).max().expect("");
+        let min_green = &self.samples.iter().map(|x| x.1).max().expect("");
+        let min_blue = &self.samples.iter().map(|x| x.2).max().expect("");
+        (*min_red, *min_green, *min_blue)
+    }
+
+    pub fn get_power(&self) -> u32 {
+        let (a, b, c) = self.get_fewest_possible();
+        a as u32 * b as u32 * c as u32
     }
 }
 
@@ -98,4 +110,10 @@ fn test_impossible() {
     let game =
         Game::new("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red");
     assert!(!game.check_if_impossible((12, 13, 14)));
+}
+
+#[test]
+fn test_min_possible() {
+    let game = Game::new("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    assert_eq!(game.get_fewest_possible(), (4, 2, 6));
 }
