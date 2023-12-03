17ad157b3f54fce57bd232cb2a2a22dca5705e33
Brad Hermens <brad.hermens@gmail.com>
2023-12-02 16:56:36 -0800

    extract game number

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -8,6 +8,39 @@ fn main() {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Game {
+    pub number: u32,
+    pub samples: Vec<(u8, u8, u8)>,
+}
 
+impl Game {
+    pub fn new(input: &str) -> Game {
+        let parts: Vec<&str> = input.split(':').collect();
+        let number = parts
+            .first()
+            .expect("")
+            .chars()
+            .filter(|x| x.is_ascii_digit())
+            .collect::<String>()
+            .parse::<u32>()
+            .expect("");
+        let samples = parts.last().expect("").split(';').collect::<Vec<&str>>();
+        Game {
+            number,
+            samples: vec![(1, 2, 3), (4, 5, 6), (7, 8, 9)],
+        }
+    }
+}
 
-
+#[test]
+fn test_new_game() {
+    let game = Game::new("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    assert_eq!(
+        game,
+        Game {
+            number: 1,
+            samples: vec![(4, 0, 3), (1, 2, 6), (0, 2, 0)]
+        }
+    );
+}
