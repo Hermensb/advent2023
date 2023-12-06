962f9bb64342fd8a8e0666d9ecc92847411e5e80
Brad Hermens <brad.hermens@gmail.com>
2023-12-06 09:21:37 -0800

    day 6 part 1 solved

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,11 +1,17 @@
 use std::{collections::HashMap, fs};
 
 fn main() {
-    let data = fs::read_to_string("data/day6").expect("Didn't find the file?");
+    let data: String = fs::read_to_string("data/day6").expect("Didn't find the file?");
+    let p1_result = part1(&data);
+    println!("Part 1 Total {}", p1_result);
+
+    let p2_result = part2(&data);
+    println!("Part 2 Total {}", p2_result);
 }
 
 fn part1(input: &str) -> u32 {
-    0
+    let data = convert_data(input);
+    data.iter().map(|x| count_win_possibilities(*x.0, *x.1)).product()
 }
 
 fn part2(input: &str) -> u32 {
@@ -15,6 +21,7 @@ fn part2(input: &str) -> u32 {
 fn convert_data(input: &str) -> HashMap<u32, u32> {
     let lines: Vec<Vec<u32>> = input
         .split("\n")
+        .filter(|x| x.len() > 0)
         .map(|x| {
             x.split(":")
                 .last()
@@ -26,6 +33,7 @@ fn convert_data(input: &str) -> HashMap<u32, u32> {
                 .collect()
         })
         .collect();
+        println!("{lines:?}");
     let pairs: HashMap<u32, u32> = (0..lines[0].len())
         .map(|x| (lines[0][x], lines[1][x]))
         .collect();
@@ -34,7 +42,7 @@ fn convert_data(input: &str) -> HashMap<u32, u32> {
 
 fn count_win_possibilities(time: u32, record: u32) -> u32 {
     let winning_runs: Vec<u32> = (0..=time)
-        .map(|x| (7 - x) * x)
+        .map(|x| (time - x) * x)
         .filter(|x| x > &record)
         .collect();
     winning_runs.len() as u32
