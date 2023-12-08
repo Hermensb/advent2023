c53528db1276742db0ca3f7f86fde4fe5fe75d6d
Brad Hermens <brad.hermens@gmail.com>
2023-12-08 09:14:00 -0800

    add day 8 files

---
+++ src/bin/day8.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+fn main() {
+    let data: String = fs::read_to_string("data/day7").expect("Didn't find the file?");
+    let p1_result = part1(&data);
+    println!("Part 1 Total {}", p1_result);
+
+    let p2_result = part2(&data);
+    println!("Part 2 Total {}", p2_result);
+}
+
+fn part1(input: &str) -> u64 {
+    0
+}
+
+fn part2(input: &str) -> u64 {
+    0
+}
