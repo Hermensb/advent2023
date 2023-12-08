a7ddd81fde9c6ed96e6c6c9f0fcc9bc6620388b4
Brad Hermens <brad.hermens@gmail.com>
2023-12-08 08:26:21 -0800

    add new test set

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -341,6 +341,30 @@ QQQJA 483"
         .to_string()
 }
 
+#[allow(dead_code)]
+fn get_more_test_data() -> String {
+    "2345A 1
+Q2KJJ 13
+Q2Q2Q 19
+T3T3J 17
+T3Q33 11
+2345J 3
+J345A 2
+32T3K 5
+T55J5 29
+KK677 7
+KTJJT 34
+QQQJA 31
+JJJJJ 37
+JAAAA 43
+AAAAJ 59
+AAAAA 61
+2AAAA 23
+2JJJJ 53
+JJJJ2 41"
+        .to_string()
+}
+
 #[test]
 fn test_part1() {
     assert_eq!(part1(&get_test_data()), 6440)
